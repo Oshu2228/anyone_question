99dc5ec2afef4e4d9c9f8981e99fbcf055d07fc1
Shunsuke Ono <ono0112sf@gmail.com>
2022-02-25 22:51:45 +0900

    #5 質問作成機能の実装

--- pages/components/QuestionList.jsx
+++ pages/components/QuestionList.jsx
@@ -1,11 +1,8 @@
 import Link from "next/link";
 import { useRecoilValue } from "recoil";
-import { EditIcon } from "@chakra-ui/icons";
+
 import {
-  Button,
-  Checkbox,
   Container,
-  Select,
   Table,
   Tbody,
   Td,
@@ -14,43 +11,10 @@ import {
   Thead,
   Tr,
 } from "@chakra-ui/react";
-
-import { todosState } from "../atoms/atom";
+import { postsState } from "../atoms/atom";
 
 const TodoList = () => {
-  const todos = useRecoilValue(todosState);
-
-  const renderStatus = (todo) => {
-    switch (todo.status) {
-      case "着手前":
-        return "orange";
-        break;
-
-      case "進行中":
-        return "skyblue";
-        break;
-
-      case "完了":
-        return "green";
-        break;
-    }
-  };
-
-  const renderPriority = (todo) => {
-    switch (todo.priority) {
-      case "低":
-        return "green";
-        break;
-
-      case "中":
-        return "yellow.400";
-        break;
-
-      case "高":
-        return "red";
-        break;
-    }
-  };
+  const posts = useRecoilValue(postsState);
 
   return (
     <>
@@ -65,24 +29,22 @@ const TodoList = () => {
           </Thead>
 
           <Tbody>
-            {todos.map((todo) => (
-              <Tr key={todo.id}>
+            {posts.map((post) => (
+              <Tr key={post.id}>
                 <Td display="flex" justifyContent="space-between" h="65.5px">
-                  
-                  <Link href={`/todos/${todo.id}`} passHref>
+                  <Link href={`/todos/${post.id}`} passHref>
                     <Text
                       cursor="pointer"
                       _hover={{ opacity: 0.7 }}
                       lineHeight="32.5px"
                     >
-                      {todo.name}
+                      {post.name}
                     </Text>
                   </Link>
-                 
                 </Td>
 
-                <Td>{todo.title}</Td>
-                <Td>{todo.createDate}</Td>
+                <Td>{post.title}</Td>
+                <Td>{post.createDate}</Td>
               </Tr>
             ))}
           </Tbody>
